3d729ebca05669ea78cc8269da3940405045287b
arvind-ishi <avij70507@gmail.com>
2024-03-21 12:53:19 +0530

    this is the new java commit

---
+++ src/files.java
@@ -0,0 +1,26 @@
+public class files {
+    public static void main(String[] args){
+
+        System.out.print("arvind jadhav ");
+
+        int muName = 234;
+        //initillization and declaration that the two types are
+        int yourName ;
+        //initillization is this
+
+        yourName = 33;
+        //declaration is this
+        System.out.println(yourName);
+
+        yourName = 44;
+         System.out.println(yourName);
+
+
+
+
+
+
+    }
+
+
+}
